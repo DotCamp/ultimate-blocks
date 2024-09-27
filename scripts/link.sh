6ebc22a5d054a29d91f14d0cf22961b09a504218
Zahin Azmayeen <zahin06@gmail.com>
2024-09-27 16:41:13 +0600

    initial commit without packages

---
+++ scripts/link.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $# -gt 1 ] ; then
+    echo "Too many arguments provided"
+    echo "USAGE: npm run link <path_to_wp_dir>"
+    exit
+fi
+
+if [ $# -eq 0 ] ; then
+    printf "Enter path to wordpress installation: "
+    read path
+else
+    path="$1"
+fi
+
+if [ ! -d "$path" ]; then
+    echo "$path does not exist."
+    exit
+fi
+
+if [ -d "packages/base" ]; then
+    base_package_dir="packages/base"
+else
+    printf "Enter path to base package: "
+    read base_package_dir
+fi
+
+if [ -d "packages/pro" ]; then
+    pro_package_dir="packages/pro"
+else
+    printf "Enter path to pro package: "
+    read pro_package_dir
+fi
+
+path=$(readlink -f "$path")
+ln -sfn $(readlink -f "$base_package_dir") "$path/wp-content/plugins/ultimate-blocks"
+ln -sfn $(readlink -f "$pro_package_dir") "$path/wp-content/plugins/ultimate-blocks-pro"
+
