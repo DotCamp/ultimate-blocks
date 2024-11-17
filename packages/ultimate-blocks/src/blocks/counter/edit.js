b7c4a8292cd9e2d2de05793efb86704899086459
farhan-shafi <farhanshafi34@gmail.com>
2024-11-17 15:13:26 +0500

    moved counter block styles to inline

--- packages/ultimate-blocks/src/blocks/counter/edit.js
+++ packages/ultimate-blocks/src/blocks/counter/edit.js
@@ -1,5 +1,5 @@
 import { __ } from "@wordpress/i18n";
-import { useState } from "@wordpress/element";
+import { isEmpty } from "lodash";
 import {
 	useBlockProps,
 	BlockControls,
@@ -8,17 +8,82 @@ import {
 } from "@wordpress/block-editor";
 
 import Inspector from "./inspector";
-import { getStyles } from "./get-styles";
+
 import { useCounter } from "./hooks/use-counter";
+import {
+	generateStyles,
+	getSpacingCss,
+	getSpacingPresetCssVar,
+} from "../utils/styling-helpers";
 
 function Edit(props) {
 	const { attributes, setAttributes } = props;
 	let counter = useCounter(attributes);
-	const { endNumber, prefix, suffix, alignment, label, labelPosition } =
-		attributes;
+	const {
+		endNumber,
+		prefix,
+		suffix,
+		alignment,
+		label,
+		labelPosition,
+		padding,
+		margin,
+	} = attributes;
+
+	const paddingObj = getSpacingCss(padding);
+	const marginObj = getSpacingCss(margin);
+	const gap = getSpacingPresetCssVar(attributes.gap?.all) ?? "";
+	const counterFontStyle = !isEmpty(attributes.counterFontAppearance.fontStyle)
+		? attributes.counterFontAppearance.fontStyle
+		: "";
+	const counterFontWeight = !isEmpty(
+		attributes.counterFontAppearance.fontWeight,
+	)
+		? attributes.counterFontAppearance.fontWeight
+		: "";
+	const labelFontStyle = !isEmpty(attributes.labelFontAppearance.fontStyle)
+		? attributes.labelFontAppearance.fontStyle
+		: "";
+	const labelFontWeight = !isEmpty(attributes.labelFontAppearance.fontWeight)
+		? attributes.labelFontAppearance.fontWeight
+		: "";
+
+	const counterWrapperStyles = {
+		gap: gap,
+	};
+	const labelStyles = {
+		color: attributes?.labelColor,
+		fontSize: attributes?.labelFontSize,
+		fontFamily: attributes.labelFontFamily,
+		textDecoration: attributes.labelDecoration,
+		lineHeight: attributes.labelLineHeight,
+		letterSpacing: attributes.labelLetterSpacing,
+		fontStyle: labelFontStyle,
+		fontWeight: labelFontWeight,
+	};
+	const counterStyles = {
+		fontSize: attributes?.counterFontSize,
+		fontFamily: attributes.counterFontFamily,
+		textDecoration: attributes.counterDecoration,
+		lineHeight: attributes.counterLineHeight,
+		"letter-spacing": attributes.counterLetterSpacing,
+		fontStyle: counterFontStyle,
+		fontWeight: counterFontWeight,
+	};
+	const containerStyles = {
+		paddingTop: paddingObj?.top,
+		paddingRight: paddingObj?.right,
+		paddingBottom: paddingObj?.bottom,
+		paddingLeft: paddingObj?.left,
+		marginTop: marginObj?.top,
+		marginRight: marginObj?.right,
+		marginBottom: marginObj?.bottom,
+		marginLeft: marginObj?.left,
+	};
+
 	const blockProps = useBlockProps({
 		className: `ub_counter-container`,
-		style: getStyles(attributes),
+		style: generateStyles(containerStyles),
 	});
 
 	return (
@@ -29,9 +94,15 @@ function Edit(props) {
 					onChange={(newValue) => setAttributes({ alignment: newValue })}
 				/>
 			</BlockControls>
-			<div className={`ub_counter ub_text-${alignment}`}>
+			<div
+				className={`ub_counter ub_text-${alignment}`}
+				style={generateStyles(counterWrapperStyles)}
+			>
 				{labelPosition === "top" && (
-					<div className="ub_counter-label-wrapper">
+					<div
+						className="ub_counter-label-wrapper"
+						style={generateStyles(labelStyles)}
+					>
 						<RichText
 							tagName="span"
 							value={label}
@@ -41,13 +112,19 @@ function Edit(props) {
 						/>
 					</div>
 				)}
-				<div className="ub_counter-number-wrapper">
+				<div
+					className="ub_counter-number-wrapper"
+					style={generateStyles(counterStyles)}
+				>
 					<span className="ub_counter-prefix">{prefix}</span>
 					<span className="ub_counter-number">{counter}</span>
 					<span className="ub_counter-suffix">{suffix}</span>
 				</div>
 				{labelPosition === "bottom" && (
-					<div className="ub_counter-label-wrapper">
+					<div
+						className="ub_counter-label-wrapper"
+						style={generateStyles(labelStyles)}
+					>
 						<RichText
 							tagName="span"
 							value={label}
