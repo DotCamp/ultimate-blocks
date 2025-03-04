03bdbdb4f7961c767c7850006b1c9c96a8d070bf
farhan-shafi <farhanshafi34@gmail.com>
2025-03-04 23:25:01 +0500

    simplify php part of image slider

--- packages/pro/src/block-extensions/image-slider/block.php
+++ packages/pro/src/block-extensions/image-slider/block.php
@@ -17,92 +17,27 @@ function ubpro_image_slider_replace_script(){
 add_action('wp_enqueue_scripts', 'ubpro_image_slider_replace_script', 20);
 
 
-function ubpro_image_slider_filter($block_content, $block){
-    if( "ub/image-slider" != $block['blockName'] ) {
-        return $block_content;
+function ubpro_image_slider_extend($slider_html, $block){
+    if ("ub/image-slider" != $block->parsed_block['blockName']) {
+        return $slider_html;
     }
-    else{
-        require dirname(dirname(__DIR__)) . '/defaults.php';
-        foreach($defaultValues['ub/image-slider']['attributes'] as $key => $value){
-            if(!isset($block['attrs'][$key])){
-                $block['attrs'][$key] = $value['default'];
-            }
-        }
 
-        extract($block['attrs']);
-        $block_attrs = $block['attrs'];
-        $margin 			 	= CSS_Generator\get_spacing_css( isset($block_attrs['margin']) ? $block_attrs['margin'] : array() );
-        $padding 			 	= CSS_Generator\get_spacing_css( isset($block_attrs['padding']) ? $block_attrs['padding'] : array() );
+    extract($block->parsed_block['attrs']);
 
-        $navigationColor = isset($block_attrs['navigationColor']) ? $block_attrs['navigationColor'] : '';
-        $activePaginationColor = isset($block_attrs['activePaginationColor']) ? $block_attrs['activePaginationColor'] : '';
-        $paginationColor = isset($block_attrs['paginationColor']) ? $block_attrs['paginationColor'] : '';
+    $imageArray = isset($pics) ? (count($pics) > 0 ? $pics : json_decode($images, true)) : array();
+    $thumbs = '';
 
-        $image_slider_wrapper_styles = array(
-            '--swiper-navigation-color'				=> $navigationColor,
-            '--swiper-pagination-color'				=> $activePaginationColor,
-            '--swiper-inactive-pagination-color'	=> $paginationColor,
-            '--swiper-navigation-background-color'	=> CSS_Generator\get_background_color_var($block_attrs, 'navigationBackgroundColor', 'navigationGradientColor'),
-            'padding-top'           			 	=> isset($padding['top']) ? $padding['top'] : "",
-            'padding-left'          			 	=> isset($padding['left']) ? $padding['left'] : "",
-            'padding-right'         			 	=> isset($padding['right']) ? $padding['right'] : "",
-            'padding-bottom'        			 	=> isset($padding['bottom']) ? $padding['bottom'] : "",
-            'margin-top'            			 	=> isset($margin['top']) ? $margin['top']  : "",
-            'margin-right'          			 	=> isset($margin['left']) ? $margin['left']  : "",
-            'margin-bottom'         			 	=> isset($margin['right']) ? $margin['right']  : "",
-            'margin-left'           			 	=> isset($margin['bottom']) ? $margin['bottom']  : "",
-            'min-height' 							=> (35 + $sliderHeight) . 'px',
-        );
+    foreach ($imageArray as $image) {
+        $thumbs .= '<img class="swiper-slide" src="' . esc_url($image['url']) . '">';
+    }
 
-	    $imageArray = isset($pics) ? (count($pics) > 0 ? $pics : json_decode($images, true)) : array();
-	    $captionArray = isset($descriptions) ? count($descriptions) > 0 ? $descriptions : json_decode($captions, true) : array();
-
-        $gallery = '';
-
-        $thumbs = '';
-
-        foreach($imageArray as $key => $image){
-            $gallery .= '<figure class="swiper-slide">
-            <img src="' . $image['url'] . '" alt="' . wp_kses_post($image['alt']) . '"' .
-                ($blockID === '' ? ' style="height: ' . $sliderHeight . 'px;"' : '') . '>' .
-                '<figcaption class="ub_image_slider_image_caption">' . ($captionArray[$key]['link'] === '' ? '' : '<a href="' . esc_url($captionArray[$key]['link']) . '">')
-                . $captionArray[$key]['text']
-                . ($captionArray[$key]['link'] === '' ? '' : '</a>') . ' </figcaption></figure>';
-
-            $thumbs .= '<img class="swiper-slide" src="' . $image['url'] . '">';
-        }
-        $classes = array( 'ub_image_slider', 'swiper-container', 'wp-block-ub-image-slider' );
-        if( !empty($align) ){
-            $classes[] = 'align' . esc_attr($align) ;
-        }
-        if(isset($className)){
-            $classes[] =  esc_attr($className) ;
-        }
-
-        return sprintf(
-            '<div class="%1$s" style="%2$s" %3$s data-swiper-data=\'{"speed":%4$d,"spaceBetween":%5$d,"slidesPerView":%6$d,"loop":%7$s,"pagination":{"el": %8$s , "type": "%9$s"%10$s},%11$s"keyboard": { "enabled": true },"effect": "%12$s"%13$s%14$s%15$s%16$s%17$s%18$s}\'>
-            <div class="swiper-wrapper">%19$s</div><div class="swiper-pagination"></div>%20$s</div>%21$s',
-            implode(' ', $classes),
-            CSS_Generator\generate_css_string($image_slider_wrapper_styles),
-            ($blockID === '' ? 'style="min-height: ' . (25 + (count($imageArray) > 0 ? $sliderHeight : 200)) . 'px;"' : 'id="ub_image_slider_' . $blockID . '"'),
-            $speed,
-            $spaceBetween,
-            $slidesPerView,
-            json_encode($wrapsAround),
-            ($usePagination ? '".swiper-pagination"' : 'null'),
-            $paginationType,
-            ($paginationType === 'bullets' ? ', "clickable":true' : ''),
-            ($useNavigation ? '"navigation": {"nextEl": ".swiper-button-next", "prevEl": ".swiper-button-prev"},' : ''),
-            $transition,
-            ($transition === 'fade' ? ',"fadeEffect":{"crossFade": true}' : ''),
-            ($transition === 'coverflow' ? ',"coverflowEffect":{"slideShadows":' . json_encode($slideShadows) . ', "rotate": ' . $rotate . ', "stretch": ' . $stretch . ', "depth": ' . $depth . ', "modifier": ' . $modifier . '}' : ''),
-            ($transition === 'cube' ? ',"cubeEffect":{"slideShadows":' . json_encode($slideShadows) . ', "shadow":' . json_encode($shadow) . ', "shadowOffset":' . $shadowOffset . ', "shadowScale":' . $shadowScale . '}' : ''),
-            ($transition === 'flip' ? ', "flipEffect":{"slideShadows":' . json_encode($slideShadows) . ', "limitRotation": ' . json_encode($limitRotation) . '}' : ''),
-            ($autoplays ? ',"autoplay":{"delay": '. ($autoplayDuration * 1000) . '}' : ''),
-            (!$isDraggable ? ',"simulateTouch":false' : ''),
-            $gallery,
-            ($useNavigation ? '<div class="swiper-button-prev"></div> <div class="swiper-button-next"></div>' : ""),
-            ($showThumbnails === true ? '<div class="ub_image_slider_thumbs swiper-container" data-swiper-data=\'{"slidesPerView": 4, "freeMode": true, "watchSlidesProgress": true, "watchSlidesVisibility": true}\'><div class="swiper-wrapper">' . $thumbs . '</div></div>' : '')
+    if ($showThumbnails === true) {
+        $slider_html .= sprintf(
+            '<div class="ub_image_slider_thumbs swiper-container" data-swiper-data=\'{"slidesPerView": 4, "freeMode": true, "watchSlidesProgress": true, "watchSlidesVisibility": true}\'><div class="swiper-wrapper">%1$s</div></div>',
+            $thumbs
         );
     }
+
+    return $slider_html;
 }
+add_filter('ubpro_image_slider_filter', 'ubpro_image_slider_extend', 10, 2);
--- packages/pro/src/blocks.php
+++ packages/pro/src/blocks.php
@@ -8,6 +8,6 @@ require_once('block-extensions/post-grid/block.php');
 
 add_filter('render_block', 'ubpro_button_filter', 10, 3);
 add_filter('render_block', 'ubpro_table_of_contents_filter', 9, 3);
-add_filter('render_block', 'ubpro_image_slider_filter', 9, 3);
+// add_filter('render_block', 'ubpro_image_slider_filter', 9, 3);
 add_filter('render_block_ub/advanced-video', 'ubpro_advanced_video_filter', 9, 3);
 add_filter('render_block_ub/post-grid', 'ubpro_render_post_grid_block', 9, 3);
